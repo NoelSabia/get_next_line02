0376f68e87603616ab1cd1b7e07b1fdc1fe995a9
Z1nnlos <Noel.Sabia@gmx.net>
2023-10-30 18:45:52 +0100

    safety push

--- get_next_line02/get_next_line.c
+++ get_next_line02/get_next_line.c
@@ -6,26 +6,47 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:07:14 by noel              #+#    #+#             */
-/*   Updated: 2023/10/30 12:35:58 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/10/30 18:43:55 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read(int start, int )
+char	*ft_read(int *start, int end, int fd)
 {
+	int		i;
+	char	*s1;
+	char	c;
 
+	i = 0;
+	s1 = (char *)ft_calloc(end - *start, sizeof(char));
+	if (!s1)
+		return (NULL);
+	while (i < end - *start && read (fd, &c, BUFFER_SIZE) > '\0')
+	{
+		s1[i] = c;
+		if (c == '\n' || c == '\0')
+			break ;
+		i++;
+		(*start)++;
+	}
+	s1[i] = '\0';
+	return (s1);
 }
 
 char	*get_next_line(int fd)
 {
-	static int	start;
+	static int	start_len;
+	static int	*start;
+	static int	end;
 	static char	*str;
 
-	start = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	start_len = 0;
+	start = &start_len;
+	end = BUFFER_SIZE;
+	if ((fd < 0 || fd > 2) || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = ft_read(start, BUFFER_SIZE);
-
+	str = ft_read(start, end, fd);
+	free (str);
 	return (str);
 }
--- get_next_line02/get_next_line.h
+++ get_next_line02/get_next_line.h
@@ -6,14 +6,15 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:07:09 by noel              #+#    #+#             */
-/*   Updated: 2023/10/30 12:22:22 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/10/30 16:05:49 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
